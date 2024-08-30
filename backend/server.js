a9f3cad07ef6454eef106cc805ed647d114c9854
Zohar Ifrah <cpxzoar@gmail.com>
2024-08-30 18:02:16 +0300

    Refactor server.js in order to upload to render.com

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const logger = require('./services/logger.service')
 // const socketService = require('./services/socket.service')
 require('dotenv').config()
@@ -14,16 +15,21 @@ const http = require('http').createServer(app)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const corsOptions = {
-    origin: [
-        'http://127.0.0.1:3001',
-        'http://localhost:3001',
-        'http://127.0.0.1:4200',
-        'http://localhost:4200'
-    ],
-    credentials: true
+// Serve static files from 'dist' in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve('dist/my-angular-app')))
+} else {
+    const corsOptions = {
+        origin: [
+            'http://127.0.0.1:3000',
+            'http://localhost:3000',
+            'http://127.0.0.1:4200',
+            'http://localhost:4200'
+        ],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 }
-app.use(cors(corsOptions))
 
 // socketService.init(http, corsOptions);
 
@@ -31,7 +37,18 @@ app.use(cors(corsOptions))
 app.use('/api/contact', contactRoutes)
 app.use('/api/user', userRoutes)
 
+// Serve index.html for all other routes
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve('dist/my-angular-app/index.html'))
+})
+
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
     logger.info('Server is running on port: ' + port)
+})
+
+// error handling middleware**
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
 })
